1ebd97e24f8262c2bad226acdea95256f9b775cb
Olybear9 <olybear9@gmail.com>
2020-07-27 03:12:55 -0500

    Chore: Push core

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # RakNet
 A fully functional RakNet implementation for Deno written in typescript.
+
+This uses the following Dependents:
+- `RaptorsMC/BinaryUtils`
+- `RaptorsMC/Math`
---
+++ lib/Listener.ts
@@ -0,0 +1,22 @@
+/**
+ *    ______            _                 ___  ________ 
+ *    | ___ \          | |                |  \/  /  __ \
+ *    | |_/ /__ _ _ __ | |_ ___  _ __ ___ | .  . | /  \/
+ *    |    // _` | '_ \| __/ _ \| '__/ __|| |\/| | |    
+ *    | |\ \ (_| | |_) | || (_) | |  \__ \| |  | | \__/\
+ *    \_| \_\__,_| .__/ \__\___/|_|  |___/\_|  |_/\____/
+ *               | |                                    
+ *               |_|      
+ * 
+ * Misuse or redistribution of this code is strictly prohibted.
+ * This applies to, and is not limited to self projects and open source projects.
+ * If you wish to use this projects code, please contact us.
+ * © RaptorsMC 2020
+ * 
+ * @author RaptorsMC
+ * @copyright https://github.com/RaptorsMC
+ * @license RaptorsMC/CustomLicense
+ */
+class Listener {
+    // Doing tomorrow
+}
